aa5f2dc4492e66e479f69fe1bf14d1bbbda3e457
JieWei-114 <jwei.l.1104@gmail.com>
2026-01-18 17:10:13 +0800

    layout

--- frontend/components/navbar.vue
+++ frontend/components/navbar.vue
@@ -1,48 +1,50 @@
 <template>
   <div class="bg-gray-800 text-white w-[15%] min-h-screen p-4 fixed top-0 left-0 flex flex-col">
-    <div class="text-2xl font-bold mb-6 py-2 px-4">Bleak Future</div>
+    <div class="text-2xl font-bold mb-6 pt-6 pb-2 pl-2">Bleak Future</div>
     <nav>
       <ul class="space-y-4">
         <li>
-          <NuxtLink to="/booking" class="block py-2 px-4 hover:bg-gray-700 rounded"
+          <NuxtLink to="/booking" class="block py-2 px-2 hover:bg-gray-700 rounded"
             >Booking</NuxtLink
           >
         </li>
         <li>
-          <NuxtLink to="/history" class="block py-2 px-4 hover:bg-gray-700 rounded"
+          <NuxtLink to="/history" class="block py-2 px-2 hover:bg-gray-700 rounded"
             >History</NuxtLink
           >
         </li>
         <li v-if="isAdminOrManager" class="relative">
           <button
             @click="toggleManageMenu"
-            class="block w-full text-left py-2 px-4 hover:bg-gray-700 rounded flex justify-between"
+            class="block w-full text-left py-2 px-2 hover:bg-gray-700 rounded flex justify-between"
           >
             Manage
             <span :class="{ 'rotate-180': manageMenuOpen }">▼</span>
           </button>
-          <ul v-show="manageMenuOpen" class="mt-2 space-y-2 bg-gray-700">
+          <ul v-show="manageMenuOpen" class="mt-2 space-y-2 bg-gray-700 rounded">
             <li>
-              <NuxtLink to="/admin/userManage" class="block py-2 px-8 hover:bg-gray-600 rounded"
+              <NuxtLink to="/admin/userManage" class="block py-2 px-4 hover:bg-gray-600 rounded"
                 >User manage</NuxtLink
               >
             </li>
             <li>
-              <NuxtLink to="/admin/buildingManage" class="block py-2 px-8 hover:bg-gray-600 rounded"
+              <NuxtLink
+                to="/admin/buildingManage"
+                class="block py-2 px-4 hover:bg-gray-600 rounded"
                 >Building manage</NuxtLink
               >
             </li>
           </ul>
         </li>
         <li>
-          <NuxtLink to="/profile" class="block py-2 px-4 hover:bg-gray-700 rounded"
+          <NuxtLink to="/profile" class="block py-2 px-2 hover:bg-gray-700 rounded"
             >Profile</NuxtLink
           >
         </li>
         <li>
           <button
             @click="handleLogout"
-            class="block w-full text-left py-2 px-4 hover:bg-gray-700 rounded hover:text-red-400"
+            class="block w-full text-left py-2 px-2 hover:bg-gray-700 rounded hover:text-red-400"
           >
             Logout
           </button>
